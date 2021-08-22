e9f0ce05edd7b73373c1e1ac990ce5bd8359f04a
Jingcheng Chen <mail@chenjingcheng.com>
2021-08-22 12:40:02 +0200

    added index file and updated tsconfig

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { Vector3d } from './Vector3d';
+export { Line } from './Line';
+export { Plane } from './Plane';
+export { Transform } from './Transform';
+export { Intersection } from './Intersection';
+
+export { ParallelIndicator, Open3d } from './Open3d';
