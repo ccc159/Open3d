f243edf5afa152045d9eca8b080d87d7ec09accf
Jingcheng Chen <mail@chenjingcheng.com>
2021-08-30 21:37:32 +0200

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'open3d.js',
+    library: {
+      type: 'umd',
+      name: 'Open3D',
+    },
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
