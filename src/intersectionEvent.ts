26bf4c87fe5ae9338b920087c776352c9f90b8ec
JonasWard <43600893+JonasWard@users.noreply.github.com>
2022-11-28 12:31:17 +0100

    intersection event class

--- src/intersectionEvent.ts
+++ src/intersectionEvent.ts
@@ -1,5 +1,8 @@
 import { Point3d } from './Point3d';
 
+/**
+ * Represents the values that describes the intersection relationship between two curve geometries.
+ */
 export class IntersectionEvent {
   ParameterA: number;
   ParameterB: number;
