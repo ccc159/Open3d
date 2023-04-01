7788120d0a7e88706f04421ac934773aa3197e8a
Jingcheng Chen <mail@jchen.ch>
2023-04-01 23:57:52 +0200

    added boundingbox test file

--- test/Boudingbox.test.ts
+++ test/Boudingbox.test.ts
@@ -1,6 +1,7 @@
 import { Point3d } from '../src/Point3d';
 import { Vector3d } from '../src/Vector3d';
 import { BoundingBox } from '../src/BoundingBox';
+import { Transform } from '../src/Transform';
 
 let p1: Point3d;
 let p2: Point3d;
@@ -66,6 +67,77 @@ test('ClosestPoint', () => {
   const closestInside = b1.ClosestPoint(inside);
   expect(closestInside.Equals(new Point3d(0.423155795, 0.952666255, 4.154620531))).toBe(true);
   const closestOnside = b1.ClosestPoint(inside, false);
-  console.log(closestOnside);
   expect(closestOnside.Equals(new Point3d(0.423155795, 0.952666255, 6.748736056))).toBe(true);
+});
+
+test('FurthestPoint', () => {
+  const outside = new Point3d(13.502516727, -1.269467385, 4.154620531);
+  const closestOutside = b1.FurthestPoint(outside);
+  expect(closestOutside.Equals(new Point3d(-9.847383153, -9.092637979, -8.103615608))).toBe(true);
+  const inside = new Point3d(0.423155795, 0.952666255, 4.154620531);
+  const closestInside = b1.FurthestPoint(inside);
+  expect(closestInside.Equals(new Point3d(-9.847383153, -9.092637979, -8.103615608))).toBe(true);
+  const p3 = new Point3d(-12, -10, -5);
+  const p3c = b1.FurthestPoint(p3);
+  expect(p3c.Equals(new Point3d(4.992788358, 6.176224321, 6.748736056))).toBe(true);
+});
+
+test('ContainsPoint', () => {
+  const outside = new Point3d(13.502516727, -1.269467385, 4.154620531);
+  const inside = new Point3d(0.423155795, 0.952666255, 4.154620531);
+  const onside = new Point3d(0.423155795, 0.952666255, 6.748736056);
+
+  expect(b1.ContainsPoint(outside)).toBe(false);
+  expect(b1.ContainsPoint(inside)).toBe(true);
+  expect(b1.ContainsPoint(onside)).toBe(true);
+  expect(b1.ContainsPoint(onside, false)).toBe(false);
+});
+
+test('ContainsBoundingBox', () => {
+  // pass
+});
+
+test('Clone', () => {
+  const b2 = b1.Clone();
+  expect(b1.Equals(b2)).toBe(true);
+  b2.Max.X = 100;
+  expect(b1.Equals(b2)).toBe(false);
+});
+
+test('PointAt', () => {
+  const p = b1.PointAt(0.5, 0.5, 0.5);
+  expect(p.Equals(new Point3d(-2.427297397, -1.458206829, -0.677439776))).toBe(true);
+  const p1 = b1.PointAt(0.1, 0.2, 0.3);
+  expect(p1.Equals(new Point3d(-8.3633660019, -6.038865519, -3.6479101088))).toBe(true);
+});
+
+test('Transform', () => {
+  const xform = Transform.Rotation(0.5, new Vector3d(1, 2, 3), new Point3d(1, 2, 3));
+  const b2 = b1.Transform(xform);
+  const corners = b2.GetCorners();
+  const expected = [
+    new Point3d(-13.283339907, -12.7657304239, -10.5399179986),
+    new Point3d(9.66788870992, -12.7657304239, -10.5399179986),
+    new Point3d(9.66788870992, 8.25586797605, -10.5399179986),
+    new Point3d(-13.283339907, 8.25586797605, -10.5399179986),
+    new Point3d(-13.283339907, -12.7657304239, 9.8342897739),
+    new Point3d(9.66788870992, -12.7657304239, 9.8342897739),
+    new Point3d(9.66788870992, 8.25586797605, 9.8342897739),
+    new Point3d(-13.283339907, 8.25586797605, 9.8342897739),
+  ];
+  for (let i = 0; i < 8; i++) {
+    expect(corners[i].Equals(expected[i])).toBe(true);
+  }
+});
+
+test('Union', () => {
+  const b2 = new BoundingBox(new Point3d(-1.23854091944787, -16.7962328401021, -3.89436309536884), new Point3d(18.9579524423731, -3.21275057905436, 3.21602902149913));
+  const b3 = b1.Union(b2);
+  expect(b3.Equals(new BoundingBox(new Point3d(-9.847383153, -16.7962328401021, -8.103615608), new Point3d(18.9579524423731, 6.176224321, 6.748736056)))).toBe(true);
+});
+
+test('Intersect', () => {
+  const b2 = new BoundingBox(new Point3d(-1.23854091944787, -16.7962328401021, -3.89436309536884), new Point3d(18.9579524423731, -3.21275057905436, 3.21602902149913));
+  const b3 = b1.Intersect(b2);
+  expect(b3.Equals(new BoundingBox(new Point3d(-1.23854091944787, -9.092637979, -3.89436309536884), new Point3d(4.992788358, -3.21275057905436, 3.21602902149913)))).toBe(true);
 });
